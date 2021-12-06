3f73f679edd95adf3f7eff9f452286b9bdfd8d1b
seonghyeokcho <63362096+seonghyeokcho@users.noreply.github.com>
2021-12-06 20:52:42 +0900

    Update opencv_supports.py

--- 02_image_processing/opencv_supports.py
+++ 02_image_processing/opencv_supports.py
@@ -1,5 +1,22 @@
 import cv2
 
+imread_flags = {
+    "color":cv2.IMREAD_COLOR,
+    "grayscale":cv2.IMREAD_GRAYSCALE,
+    "unchanged":cv2.IMREAD_UNCHANGED
+    }
+
+def load_image(src, flag="color"):
+    if flag not in imread_flags.keys():
+        print(f"This \"{flag}\" flag key is not in imread_flags dictionary.")
+        return
+    
+    matrix = cv2.imread(
+        "/Users/csh/programming_language/001_Python/02_image_processing/images/" + src,
+        imread_flags[flag]
+        )
+    return matrix
+
 def close_opencv_window_for_macOS():
     cv2.waitKey()
     cv2.destroyAllWindows()
