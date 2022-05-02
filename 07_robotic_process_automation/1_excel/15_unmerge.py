ce35df3e92d7af97692d53cac2aa135eea0bac03
seonghyeokcho <63362096+seonghyeokcho@users.noreply.github.com>
2022-05-03 00:06:25 +0900

    remove

--- 07_robotic_process_automation/1_excel/15_unmerge.py
+++
@@ -1,8 +0,0 @@
-from openpyxl import load_workbook
-wb = load_workbook("/Users/csh/jupytercreation/Python_Practice/RPA/basic/1_excel/sample_merge.xlsx")
-ws = wb.active
-
-# B2:D2 병합되어 있던 셀을 헤제
-ws.unmerge_cells('B2:D2')
-
-wb.save("/Users/csh/jupytercreation/Python_Practice/RPA/basic/1_excel/sample_unmerge.xlsx")
--- 07_robotic_process_automation/2_desktop/6_image_recognition.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-
-file_menu = pyautogui.locateOnScreen("/Users/csh/jupytercreation/Python_Practice/RPA/basic/2_desktop/img/file_menu.png")
-print(file_menu)
-# pyautogui.click(file_menu)
-pyautogui.moveTo(12,112)
