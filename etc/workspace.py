fea51a2bae5f0a29499e34aa25f4134839795b4e
seonghyeokcho <63362096+seonghyeokcho@users.noreply.github.com>
2022-09-02 08:26:31 +0900

    Update workspace.py

--- etc/workspace.py
+++ etc/workspace.py
@@ -4,18 +4,18 @@ from bs4 import BeautifulSoup as bs4
 
 headers = {"User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'}
 
-websites = [
-    "google.com",
-    "airbnb.com",
-    "twitter.com",
-    "facebook.com"
-]
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
+search_term = "python"
 
-for website in websites:
-    if not website.startswith("http://"):
-        website = f"http://{website}"
-    # result = requests.get(website)
-    # result.raise_for_status()
-    print(website)
-    
-    
+response = requests.get(f"{base_url}{search_term}", headers=headers)
+response.raise_for_status()
+
+soup = bs4(response.text, "html.parser")
+jobs = soup.find_all("section", class_="jobs")
+
+for job_section in jobs:
+    job_posts = job_section.find_all("li")
+    job_posts.pop()
+    for post in job_posts:
+        print(post)
+        print("/"*20)
