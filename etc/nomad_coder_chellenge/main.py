fe9925d545097f6d9a59e6d1d004805aee7be44c
seonghyeokcho <63362096+seonghyeokcho@users.noreply.github.com>
2022-09-03 00:36:31 +0900

    Update main.py

--- etc/nomad_coder_chellenge/main.py
+++ etc/nomad_coder_chellenge/main.py
@@ -1,25 +1,42 @@
-import requests
 from bs4 import BeautifulSoup as bs4
+
+# How to open browser for selenium 4.x version
 from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 from extractors.wwr import extract_wwr_jobs
 
 
-# headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
-# url = "https://search.incruit.com/list/search.asp?col=all&src=gsw*www&kw="
-# search_term = "python"
-# response = requests.get(f"{url}{search_term}", headers=headers)
-# response.raise_for_status()
+url = "https://kr.indeed.com/jobs?q="
+search_term = "python"
+browser = Chrome(service=Service(ChromeDriverManager().install()))
+browser.get(f"{url}{search_term}")
 
-browser = Chrome("/Users/csh/programming_language/001_Python/etc/nomad_coder_chellenge/chromedriver")
-url = "https://www.indeed.com/jobs?q=python&limit=50"
-browser.get(url)
+soup = bs4(browser.page_source, "html.parser")
+job_list = soup.find("ul", class_="jobsearch-ResultsList")
+jobs = job_list.find_all("li", recursive=False)
+results = []
 
-# soup = bs4(response.text, "html.parser")
-# job_list = soup.find("div", class_="cBbslist_contenst")
-# jobs = job_list.find_all("ul")
+for job in jobs:
+    zone = job.find("div", class_="mosaic-zone")
+    if zone == None:
+        anchor = job.select_one("h2 a")
+        title = anchor["aria-label"]
+        link = anchor["href"]
+        company = job.find("span", class_="companyName")
+        location = job.find("div", class_="companyLocation")
+        job_data = {
+            "link": f"https://kr.indeed.com{link}",
+            "company": company.string,
+            "location": location.string,
+            "position": title
+        }
+        results.append(job_data)
 
-# for job in jobs:
-#     print(job)
-#     print("="*50)
-#     print("="*50)
+for result in results:
+    print(result)
+    print("="*30)
+    print("="*30)
