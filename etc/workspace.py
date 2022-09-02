9afd7aa7657fe6c882debe2e8312ed33c0308ea1
seonghyeokcho <63362096+seonghyeokcho@users.noreply.github.com>
2022-09-02 11:56:42 +0900

    Update workspace.py

--- etc/workspace.py
+++ etc/workspace.py
@@ -10,6 +10,7 @@ search_term = "python"
 response = requests.get(f"{base_url}{search_term}", headers=headers)
 response.raise_for_status()
 
+results = []
 soup = bs4(response.text, "html.parser")
 jobs = soup.find_all("section", class_="jobs")
 
@@ -17,5 +18,19 @@ for job_section in jobs:
     job_posts = job_section.find_all("li")
     job_posts.pop()
     for post in job_posts:
-        print(post)
-        print("/"*20)
+        anchor = post.find_all("a")[1]
+        link = anchor["href"]
+        company, _, region = anchor.find_all("span", class_="company")
+        title = anchor.find("span", class_="title")
+        # print(company.string, region.string, title.string)
+        job_data = {
+            "company": company.string,
+            "region": region.string,
+            "position": title.string
+        }
+        results.append(job_data)
+        
+print("="*60)
+for result in results:
+    print(result)
+    print("="*30)
