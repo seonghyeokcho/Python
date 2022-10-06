dbcfd7b7b97d92dc679e550ede4a5e0267e734ef
seonghyeokcho <whtjdgur77@gmail.com>
2022-10-07 01:06:13 +0900

    Update indeed.py

--- etc/nomad_coder_chellenge/extractors/indeed.py
+++ etc/nomad_coder_chellenge/extractors/indeed.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup as bs4
 import requests
 
 # How to open browser for selenium 4.x version
-from selenium.webdriver import Chrome
+from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -10,7 +10,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def get_page_count(keyword):
     url = "https://kr.indeed.com/jobs?q="
-    browser = Chrome(service=Service(ChromeDriverManager().install()))
+    browser = Chrome(service=Service(ChromeDriverManager().install()), options=set_webdriver_options())
     browser.get(f"{url}{keyword}")
     soup = bs4(browser.page_source, "html.parser")
     pagination = soup.find("ul", class_="pagination-list")
@@ -27,8 +27,22 @@ def get_page_count(keyword):
         return count
 
 
+def set_webdriver_options():
+    options = ChromeOptions()
+    options.headless = True
+    options.add_argument("window-size=2880x1800")
+    options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36")
+    return options
+
+
+def print_job_info(data):
+    for k, v in data.items():
+        print(f"{k}: {v}")
+    print("="*30)
+
 def extract_indeed_jobs(keyword):
     results = []
+    
     pages = get_page_count(keyword)
     print(f"Found {pages} pages")
     
@@ -36,12 +50,18 @@ def extract_indeed_jobs(keyword):
         url = "https://kr.indeed.com/jobs"
         final_url = f"{url}?q={keyword}&start={page*10}"
         print(f"Requesting {final_url}")
-        browser = Chrome(service=Service(ChromeDriverManager().install()))
+        
+        # call Chrome browser
+        browser = Chrome(service=Service(ChromeDriverManager().install()), options=set_webdriver_options())
         browser.get(final_url)
+        
+        # make beautiful soup
         soup = bs4(browser.page_source, "html.parser")
+        
+        # extract job data
         job_list = soup.find("ul", class_="jobsearch-ResultsList")
         jobs = job_list.find_all("li", recursive=False)
-
+        print("="*30)
         for job in jobs:
             zone = job.find("div", class_="mosaic-zone")
             
@@ -58,4 +78,9 @@ def extract_indeed_jobs(keyword):
                     "position": title.replace(",", " "),
                 }
                 results.append(job_data)
+                print_job_info(job_data)  # print job data
     return results
+
+if __name__ == "__main__":
+    term = input("What do you want search for?: ")
+    extract_indeed_jobs(term)
