ef0971086614122a0f044fb35863f5b25ae80778
seonghyeokcho <whtjdgur77@gmail.com>
2022-12-25 08:39:45 +0900

    migration
    migration

--- 11_kalman_filter/source/11.ARS/EulerAccel2.m
+++
@@ -1,7 +0,0 @@
-function [phi, theta] = EulerAccel2(ax, ay, az)
-%
-%
-
-phi   = atan(ay/az);
-theta = atan(ax/sqrt(ay^2 + az^2));
- 
--- 11_kalman_filter/source/12.EKF/EulerEKF/EulerAccel.m
+++
@@ -1,9 +0,0 @@
-function [phi, theta] = EulerAccel(ax, ay, az)
-%
-%
-g = 9.8;
-
-theta = asin(  ax / g );
-phi   = asin( -ay / (g*cos(theta)) );
-
- 
