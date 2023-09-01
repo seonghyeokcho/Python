330fe57b373c1d2b59d597ff9990d46655e98bee
seonghyeokcho <whtjdgur77@gmail.com>
2023-09-02 01:39:29 +0900

    Update increase_prop.py

--- etc/increase_prop.py
+++ etc/increase_prop.py
@@ -15,23 +15,28 @@ def optimized_loop(boxes, numbers):
                 break
         else:
             break
-
+    
     return count
 
-prisoner_numbers = 100
-trials = 10000
-success_count = 0
 
-for _ in range(trials):
-    boxes = list(range(1, prisoner_numbers + 1))
+# EPOCHS = 1000
+average_rates = []
+TRIALS = 1000000
+PRISONERS = 100
+
+# for epoch in range(EPOCHS):
+success_count = 0
+boxes = list(range(1, PRISONERS + 1))
+for _ in range(TRIALS):
     numbers = boxes.copy()
     random.shuffle(numbers)
     result = optimized_loop(boxes, numbers)
-    if result == prisoner_numbers:
+    if result == PRISONERS:
         success_count += 1
-
-success_rate = success_count / trials
-
-print(f"Success: {success_count}")
-print(f"Failure: {trials - success_count}")
-print(f"Success Rate: {success_rate}")
+success_rate = success_count / TRIALS
+# average_rates.append(success_rate)
+    
+    # if epoch % 10 == 0:
+    #     print(f"Epoch: {epoch} | Success: {success_count} | Failure: {TRIALS - success_count} | Success Rate: {success_rate}")
+# print(f"Average Success Rate for {EPOCHS} Epochs: {sum(average_rates)/len(average_rates)}")
+print(f"Trials: {TRIALS} | Success: {success_count} | Failure: {TRIALS - success_count} | Success Rate: {success_rate}")
